1e47f4d0eff8e232e173bf75d20f4edff4f817ee
Rahulk8439 <rahul.sharma20030158@gmail.com>
2021-08-16 14:55:06 +0530

    restaurent-menu (react.js)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Restaurent from "./component/Basics/Restaurent";
+import UseState from "./component/Hooks/useState.js";
+
+const App = () => {
+  return (
+    <>
+      <UseState />
+    </>
+  );
+};
+
+export default App;
